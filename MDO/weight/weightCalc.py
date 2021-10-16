Fix weight raymer calculation

MDO/weight/weightCalc.py
@@ -1,20 +1,23 @@
 """
 Weight estimation
 """
+import os
+
 import numpy as np
 
 
-def weightCalc(aircraftInfo, method="Raymer"):
+def weightCalc(aircraftInfo, weightInfo=None, method="Raymer"):
     """
 
     """
+    # Wing
     wingArea = aircraftInfo.wing.area
     aileronArea = aircraftInfo.wing.aileronArea
     arWing = aircraftInfo.wing.span ** 2 / wingArea
     tcRootWing = aircraftInfo.tcRootWing
     taperRatioWing = aircraftInfo.wing.taperRatio
     wingSweep = aircraftInfo.wing.sweep
-    mtow = aircraftInfo.weight.initialMTOW
+    mtow = weightInfo.initialMTOW
     wingMeanChord = aircraftInfo.wing.meanChord
     xWingMeanChord = aircraftInfo.wing.xMeanChord
 
@@ -22,54 +25,97 @@ def weightCalc(aircraftInfo, method="Raymer"):
     horizontalArea = aircraftInfo.horizontal.area
     xHorizontalMeanChord = aircraftInfo.horizontal.xMeanChord
     horizontalMeanChord = aircraftInfo.horizontal.meanChord
+    xHorizontalRoot = aircraftInfo.horizontal.rootX
 
     # Vertical
     verticalArea = aircraftInfo.vertical.area
     xVerticalMeanChord = aircraftInfo.vertical.xMeanChord
     verticalMeanChord = aircraftInfo.vertical.meanChord
+    xVerticalRoot = aircraftInfo.vertical.rootX
 
     # Fuselage
     fuselageWetArea = aircraftInfo.fuselage.wetArea
     fuselageLength = aircraftInfo.fuselage.length
 
-    def _raymerWing():
-        Nz = 1.5 * 2.5
-        wingAreaBtu = wingArea * 10.7639  # Wing Area in ft^2.
-        controlAreaBtu = aileronArea * 10.7639  # Control Surface Area in ft^2.
-        mto6Btu = mtow * 0.224809  # lbf
-        wingWeightBtu = 0.0051 * (mto6Btu * Nz) ** 0.557 * wingAreaBtu ** 0.649 \
-                        * arWing ** 0.55 * tcRootWing ** (-0.4) \
-                        * (1 + taperRatioWing) ** 0.1 * \
-                        (np.cos(wingSweep)) ** (-1) * controlAreaBtu ** 0.1
-        wingWeight = wingWeightBtu / 0.224809  # Wing weight in N.
+    # Cruise
+    q = 1/2*(1*0.0765)*(20*3.28084)**2  # TODO: Dynamic pressure at cruise
 
+    def _raymerWing(method_calc='General Aviation'):
+        """Calculated with formula from Raymer new book page 576"""
+        Nz = 1.5 * 2.5
+        wingAreaFt = wingArea * 10.7639  # wingAreaLb: Wing Area in ft^2.
+
+        if method_calc == 'General Aviation':
+            mto6Btu = mtow * 0.224809  # lbf
+            wingWeightBtu = 0.036 * wingAreaFt ** 0.758 * (arWing/(np.cos(wingSweep)**2)) ** 0.6 \
+                            * q ** 0.006 * taperRatioWing ** 0.04 \
+                            * (100*tcRootWing/(np.cos(wingSweep))) ** (-0.3) \
+                            * (mto6Btu * Nz) ** 0.49
+
+            wingWeightBtu = 2* wingWeightBtu # TODO: This is a correction carteada to make sense.
+        elif method_calc == 'Cargo Transport':
+            controlAreaBtu = aileronArea * 10.7639  # Control Surface Area in ft^2.
+            mto6Btu = mtow * 0.224809  # lbf
+            wingWeightBtu = 0.0051 * (mto6Btu * Nz) ** 0.557 * wingAreaFt ** 0.649 \
+                            * arWing ** 0.55 * tcRootWing ** (-0.4) \
+                            * (1 + taperRatioWing) ** 0.1 * \
+                            (np.cos(wingSweep)) ** (-1) * controlAreaBtu ** 0.1
+
+        wingWeight = wingWeightBtu / 0.224809  # wingWeight: Wing weight in N.
         xCgWing = xWingMeanChord + 0.4 * wingMeanChord
+
+        if os.environ["DEBUG"].lower() == 'yes':
+            print("-"*10)
+            print('Wing Weight and CG: ', wingWeight, xCgWing)
+            print(f'Wing area: {wingArea}')
+            print(f'Weight/Area: {wingWeight/wingArea/9.81}')
+
         return wingWeight, xCgWing
 
-    def _raymerStabSurfaces(surfaceArea, meanChord, xMeanChord):
-        weight = 27 * 9.81 * surfaceArea
-        xcg = xMeanChord + 0.4 * meanChord
+    def _raymerStabSurfaces(surfaceArea, meanChord, xMeanChord, xRoot):
+        weight = 10 * 9.81 * surfaceArea
+        xcg = xRoot + xMeanChord + 0.4 * meanChord
+
+        if os.environ["DEBUG"].lower() == 'yes':
+            print("-" * 10)
+            print('Horizontal or Vertical Weight and CG: ', weight, xcg)
+            print(f'Weight/Area: {weight / surfaceArea / 9.81}')
         return weight, xcg
 
     def _raymerFuselage():
-        Wf = 24 * 9.81 * fuselageWetArea
+        Wf = 7 * 9.81 * fuselageWetArea
         xcgf = 0.45 * fuselageLength
+        if os.environ["DEBUG"].lower() == 'yes':
+            print("-" * 10)
+            print('Fuselage Weight and CG: ', Wf, xcgf)
+            print(f'Weight/Area: {Wf / fuselageWetArea / 9.81}')
         return Wf, xcgf
 
     def _raymerLandingGear(W0, xCG, loadPercentage, weightFactor=0.043):
         weightLandingGear = loadPercentage * weightFactor * W0
+        if os.environ["DEBUG"].lower() == 'yes':
+            print("-" * 10)
+            print('Landing Weight and CG: ', weightLandingGear, xCG)
         return weightLandingGear, xCG
 
     def _engineWeight():
-        return aircraftInfo.weight.engine, aircraftInfo.cg.engine[0]
+        if os.environ["DEBUG"].lower() == 'yes':
+            print("-" * 10)
+            print('Engine Weight and CG: ', weightInfo.engine, aircraftInfo.cg.engine[0])
+        return weightInfo.engine, aircraftInfo.cg.engine[0]
 
     def _allElseWeight():
-        return sum([v[0] for v in aircraftInfo.weight.allElse.values()]), sum([v[1] for v in aircraftInfo.weight.allElse.values()])
+        allElseWeight = sum([v[0] for v in weightInfo.allElse.values()])
+        allElseWeightCG = sum([v[1] for v in weightInfo.allElse.values()])
+        if os.environ["DEBUG"].lower() == 'yes':
+            print("-" * 10)
+            print('All else Weight and CG: ', allElseWeight, allElseWeightCG)
+        return allElseWeight, allElseWeightCG
 
     methods = {
         "Raymer": [_raymerWing(),
-                   _raymerStabSurfaces(horizontalArea, horizontalMeanChord, xHorizontalMeanChord),
-                   _raymerStabSurfaces(verticalArea, verticalMeanChord, xVerticalMeanChord),
+                   _raymerStabSurfaces(horizontalArea, horizontalMeanChord, xHorizontalMeanChord, xHorizontalRoot),
+                   _raymerStabSurfaces(verticalArea, verticalMeanChord, xVerticalMeanChord, xVerticalRoot),
                    _raymerFuselage(),
                    _raymerLandingGear(mtow, aircraftInfo.xNoseLG, 0.15),
                    _raymerLandingGear(mtow, aircraftInfo.xMainLG, 0.85),

main.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     # ----Config ----------------------------------------------
     MDO.parseConfig("outputsConfig.cfg")
 
-    num_states = 10
+    num_states = 11
     x_states = np.array([0.0] * num_states)
 
     x_states[0] = 6  # wingSpan = 6